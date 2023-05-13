7cb072e459561e7d8d9019bc926a5682b323b0fd
Carlo C <carlo.cruz0215@gmail.com>
2023-05-13 09:06:13 +0800

    PHP

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width = device-width, initial-scale = 1">
+  <title>777 The Light</title>
+
+  <!-- add favicon -->
+  <link rel="icon" type="image/png" href="img/favicon.png">
+
+  <!-- add css -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  
+
+</head>
+<body>
+
+  
+
+  <!-- header markup -->
+
+  <header>
+    <div class="container">
+      <div class="logo">
+        <h2><a href="#">777 The Light</a></h2>
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#"> (123) 456-7890</a></li>
+          <li><a href="#">test@testemail.com</a></li>
+        </ul>
+      </nav>      
+    </div>
+  </header>
+
+  <!-- hero section markup -->
+
+  <section class="hero">
+    <div class="container">
+      <div class="image">
+        <img src="img/headerbulb.png">
+      </div>
+
+      <div class="details">
+        <div class="top">
+          <h2>777 The Light<br>  <span> – designer’s discretion </span> </h2>
+          <p> (123) 456-7890</p>
+          <p>test@testemail.com</p>
+        </div>
+
+       
+      </div>
+    </div>
+  </section>
+
+  <!-- service section markup -->
+
+  <section class="services">
+    <div class="container">
+      <div class="left">
+        <h2>Fresh Rhymes<br>  <span>Sick Beats</span> </h2>
+          <p> Producing Creative and Original Hip-Hop Music</p>
+
+        <div class="action">
+          <a href="#join">Listen now</a>
+        </div>
+      </div>
+
+      <div class="right">
+        <div class="center">            
+          <img src="img/man1.jpg">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- testimonials section markup -->
+
+  <section class="testimonials">
+    <div class="container">
+      
+
+      <div class="box">
+        <div class="left">
+          <img src="img/man2.jpg">
+        </div>
+
+        <div class="right">
+          <div class="quote">
+            <h1>A Talented Rap Artist With a Unique Sound</h1>
+            <p>My name is 777 The light, and I have been in the music industry for more than 11 years. I work hard to create upbeat and distinct hip-hop songs and instrumentals for many people in Northern California.</p>
+            <br>
+          </div>
+          <div class="quote">
+            <h2>Giving You Free Music</h2>
+            <p>My song “Top of the Bay” is ready to be your summer soundtrack. Listen to it now and enjoy its expressive lyrics and lush melodies. </p>
+          <br>
+          </div>
+           <div class="quote">
+            <h2>A Sample of My Work </h2>
+            <p>It is such a delight to share with you what I do. I will soon put out MP3 versions of my compositions for everyone to download, so be sure to check back for updates. </p>
+          <br>
+          </div>
+
+           <div class="quote">
+            <h2>Get in Touch With Me</h2>
+            <p>For any questions or concerns about my music, reach out to me today. I am always ready to provide you with the answers you need. </p>
+          <br>
+          </div>
+
+
+        </div> 
+      </div>
+    </div>
+  </section>
+
+  <!-- join section markup -->
+<span id = "join"> </span>
+  <section class="join">
+    <div class="container">
+      <div class="heading">
+        <h1>777 The light Would Love to Hear From You</h1>
+        <p>Leave your name and email below and someone will contact you soon.
+</p>
+      </div>
+
+      <div class="action">
+        <form>
+          <input type="text" name="name" id="name" placeholder="Your Name" autocomplete="off"><br>
+          <input type="email" name="email" id="mail" placeholder="Your Email" autocomplete="off"><br>
+          <input type="number" name="phone" id="phone" placeholder="Your Number" autocomplete="off"><br>
+          <input type="submit" name="submit" value="Submit">
+          
+
+        </form>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer markup -->
+
+  <footer>
+    <div class="container">
+      <img src="img/bulb.png">
+
+      <p>Copyright &copy; 2021 - <a href="#"> 777 The Light</a> All Right Reserved</p>
+    </div>
+  </footer>
+
+  <!-- add javascript -->
+  <script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
